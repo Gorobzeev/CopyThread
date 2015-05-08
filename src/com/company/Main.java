241314d60a6fe36271440416bcd07854ccadaaaa
Gorobzeev <staszp@gmail.com>
2015-05-09 01:50:48 +0300

    second

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
 
-        String dir1 = "D://MyProjects//CopyThread//old";
-        String dir2 = "D://MyProjects//CopyThread//new";
+        String dir1 = args[0];
+        String dir2 = args[1];
         copyFilesDir(dir1,dir2);
 
     }
