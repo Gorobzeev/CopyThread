e99fe98de8c90ce575f98b220e39ad1a5fd0cc3d
Gorobzeev <staszp@gmail.com>
2015-05-09 01:27:51 +0300

    run

---
+++ src/com/company/RunCopy.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class  RunCopy implements Runnable {
+    private final File out;
+    private final File in;
+
+    public RunCopy (File in, File out){
+        this.in = in;
+        this.out = out;
+
+    }
+
+    @Override
+    public void run() {
+        byte buffer[] = new byte[100000000];
+        try {
+            FileInputStream fileIn = new FileInputStream(in);
+            int bytes = fileIn.read(buffer,0,100000000);
+            fileIn.close();
+
+            FileOutputStream fileOut = new FileOutputStream(out);
+            fileOut.write(buffer,0,bytes);
+            fileOut.close();
+        }
+        catch (Exception e) {
+
+        }
+    }
+}
